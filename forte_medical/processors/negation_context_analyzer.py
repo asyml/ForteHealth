b9d9aa2e60a55d76dbf4c99c8bf2d5ae2ff08a75
Piyush Yadav <piyush13y@gmail.com>
2022-03-01 19:37:53 -0800

    rectified black format issues

--- forte_medical/processors/negation_context_analyzer.py
+++ forte_medical/processors/negation_context_analyzer.py
@@ -50,10 +50,10 @@ class NegationContextAnalyzer(PackProcessor):
         sorted_rules = []
 
         for rule in rule_list:
-            rule_tokens = rule.strip().split('\t')
+            rule_tokens = rule.strip().split("\t")
             rule_phrase_tokens = rule_tokens[0].split()
-            rule_phrase = r'\s+'.join(rule_phrase_tokens)
-            pattern = r'\b' + '(' + rule_phrase + ')' + r'(?!(\[PREN\])|(\[POST\]))\b'
+            rule_phrase = r"\s+".join(rule_phrase_tokens)
+            pattern = r"\b(" + rule_phrase + r")(?!(\[PREN\])|(\[POST\]))\b"
             rule_tokens.append(pattern)
             sorted_rules.append(rule_tokens)
 
@@ -61,15 +61,20 @@ class NegationContextAnalyzer(PackProcessor):
 
     def set_up(self, configs: Config):
         if len(configs.negation_rules_path) > 0:
-            with open(configs.negation_rules_path, 'r', encoding='utf8') as rules_file:
+            with open(
+                configs.negation_rules_path, "r", encoding="utf8"
+            ) as rules_file:
                 all_rules = rules_file.readlines()
                 all_rules.extend(configs.pre_negation_rules)
                 all_rules.extend(configs.post_negation_rules)
                 self.__rules = self.__sort_rules(all_rules)
         else:
-            raise ProcessExecutionException("Please provide a file path in config as "
-                            + "config.negation_rules_path, for negation rules that will "
-                            + "be used by the processor.")
+            raise ProcessExecutionException(
+                "Please provide a file path in "
+                + "config as config.negation_rules_path, for "
+                + "negation rules that will be used by the "
+                + "processor."
+            )
 
     def initialize(self, resources: Resources, configs: Config):
         super().initialize(resources, configs)
@@ -77,9 +82,10 @@ class NegationContextAnalyzer(PackProcessor):
 
     def _process(self, input_pack: DataPack):
         r"""
-        Negation Context analysis is done on the basis of tagging entities and detecting phrases.
-        We use the negation rules' file to set all rule phrases which would convey a negative
-        context depending on their positioning, before or after an entity.
+        Negation Context analysis is done on the basis of tagging entities and
+        detecting phrases. We use the negation rules' file to set all rule
+        phrases which would convey a negative context depending on their
+        positioning, i.e. before or after an entity.
 
         Rules should be in a tab-delimited format: 'rule_phrase\t\t[Tag]'
         Types of Tags:
@@ -93,36 +99,42 @@ class NegationContextAnalyzer(PackProcessor):
         does not exist  [POST]
         but             [CONJ]
 
-        The first rule implies, if the phrase 'absence of' is present in the sentence before
-        an entity, we annotate the entity as negation polarity=True. Similarly, if the phrase
-        'does not exist' occurs in the sentence after an entity, it implies negative context.
-        We also handle conjunctions, based off of rules from the rules' file.
+        The first rule implies, if the phrase 'absence of' is present in the
+        sentence before an entity, we annotate the entity as negation
+        polarity=True. Similarly, if the phrase 'does not exist' occurs in the
+        sentence after an entity, it implies negative context. We also handle
+        conjunctions, based off of rules from the rules' file.
         For example, given a sentence:
 
         "CT scan shows xyz but absence of lesions."
-        Our implementation would set negation polarity for 'xyz' as False, but won't carry it to
-        'lesions' because we detect 'but' as a conjunction in the sentence. Hence, we decide
-        polarity for 'lesions' independently as True given the presence of 'absence' phrase before
-        'lesions'.
-        All rule phrases are tagged in the sentence as [PREN]..[PREN], [POST]..[POST] or
-        [CONJ]..[CONJ], depending on how they are defined in the rules' file. All entity mentions
-         are first annotated with the [ENTITY]..[ENTITY] tags.
+        Our implementation would set negation polarity for 'xyz' as False, but
+        won't carry it to 'lesions' because we detect 'but' as a conjunction in
+        the sentence. Hence, we decide polarity for 'lesions' independently as
+        True given the presence of 'absence' phrase before 'lesions'.
+
+        All rule phrases are tagged in the sentence as [PREN]..[PREN],
+        [POST]..[POST] or [CONJ]..[CONJ], depending on how they are defined in
+        the rules' file. All entity mentions are first annotated with the
+        [ENTITY]..[ENTITY] tags.
         On negation detection, we replace those with [NEGATED]..[NEGATED].
-        These tags are then used to define NegationContext annotations in the `input_pack` with
-        corresponding polarity.
+        These tags are then used to define NegationContext annotations in the
+        `input_pack` with corresponding polarity.
         """
 
         for sentence in input_pack.get(Sentence):
-            filler = '_'
+            filler = "_"
             tagged_sentence = sentence.text
             for rule in self.__rules:
-                reformat_rule = re.sub(r'\s+', filler, rule[0].strip())
-                tagged_sentence = re.sub(rule[3], rule[2].strip()
-                                                    + reformat_rule
-                                                    + rule[2].strip(),
-                                                tagged_sentence)
+                reformat_rule = re.sub(r"\s+", filler, rule[0].strip())
+                tagged_sentence = re.sub(
+                    rule[3],
+                    rule[2].strip() + reformat_rule + rule[2].strip(),
+                    tagged_sentence,
+                )
 
-            entities = [em.text for em in input_pack.get(EntityMention, sentence)]
+            entities = [
+                em.text for em in input_pack.get(EntityMention, sentence)
+            ]
             entities = set(entities)
 
             if len(entities) == 0:
@@ -130,18 +142,21 @@ class NegationContextAnalyzer(PackProcessor):
 
             for entity in entities:
 
-                # Precede all ].,?+(){^* with a '\' so the special characters dont interfere
-                # with the regex execution.
-                entity = re.sub(r'([.^$*+?{\\|()[\]])', r'\\\1', entity)
+                # Precede all ].,?+(){^* with a '\' so the special characters
+                # dont interfere with the regex execution.
+                entity = re.sub(r"([.^$*+?{\\|()[\]])", r"\\\1", entity)
                 split_entity = entity.split()
-                joiner = r'\W+'
-                pattern = r'\b' + joiner.join(split_entity) + r'\b'
+                joiner = r"\W+"
+                pattern = r"\b" + joiner.join(split_entity) + r"\b"
                 entityPattern = re.compile(pattern, re.IGNORECASE)
                 match = entityPattern.search(tagged_sentence)
                 if match:
-                    tagged_sentence = tagged_sentence.replace(match.group(0), '[ENTITY]'
-                                        + re.sub(r'\s+', filler, match.group(0).strip())
-                                        + '[ENTITY]')
+                    tagged_sentence = tagged_sentence.replace(
+                        match.group(0),
+                        "[ENTITY]"
+                        + re.sub(r"\s+", filler, match.group(0).strip())
+                        + "[ENTITY]",
+                    )
 
             overlap_flag = 0
             pre_negation_flag = 0
@@ -150,55 +165,63 @@ class NegationContextAnalyzer(PackProcessor):
             sentence_tokens = tagged_sentence.split()
 
             for i, _ in enumerate(sentence_tokens):
-                if sentence_tokens[i][:6] == '[PREN]':
+                if sentence_tokens[i][:6] == "[PREN]":
                     pre_negation_flag = 1
                     overlap_flag = 0
 
-                if sentence_tokens[i][:6] in ['[CONJ]', '[POST]']:
+                if sentence_tokens[i][:6] in ["[CONJ]", "[POST]"]:
                     overlap_flag = 1
 
                 if pre_negation_flag == 1 and overlap_flag == 0:
-                    sentence_tokens[i] = sentence_tokens[i].replace('[ENTITY]', '[NEGATED]')
+                    sentence_tokens[i] = sentence_tokens[i].replace(
+                        "[ENTITY]", "[NEGATED]"
+                    )
 
             sentence_tokens.reverse()
 
             for i, _ in enumerate(sentence_tokens):
-                if sentence_tokens[i][:6] == '[POST]':
+                if sentence_tokens[i][:6] == "[POST]":
                     post_negation_flag = 1
                     overlap_flag = 0
 
-                if sentence_tokens[i][:6] in ['[CONJ]', '[PREN]']:
+                if sentence_tokens[i][:6] in ["[CONJ]", "[PREN]"]:
                     overlap_flag = 1
 
                 if post_negation_flag == 1 and overlap_flag == 0:
-                    sentence_tokens[i] = sentence_tokens[i].replace('[ENTITY]', '[NEGATED]')
+                    sentence_tokens[i] = sentence_tokens[i].replace(
+                        "[ENTITY]", "[NEGATED]"
+                    )
 
             sentence_tokens.reverse()
-            tagged_sentence = ' '.join(sentence_tokens)
-            tagged_sentence = tagged_sentence.replace(filler, ' ')
+            tagged_sentence = " ".join(sentence_tokens)
+            tagged_sentence = tagged_sentence.replace(filler, " ")
 
-            r = re.compile(r'(\[NEGATED\][\w|\s]*\[NEGATED\])')
+            r = re.compile(r"(\[NEGATED\][\w|\s]*\[NEGATED\])")
             neg_matches = r.findall(tagged_sentence)
 
-            r = re.compile(r'(\[ENTITY\][\w|\s]*\[ENTITY\])')
+            r = re.compile(r"(\[ENTITY\][\w|\s]*\[ENTITY\])")
             pos_matches = r.findall(tagged_sentence)
 
             for match in pos_matches:
-                substring = re.sub(r'(\[\w*\])', '', match)
-                pattern = r'\b' + substring + r'\b'
+                substring = re.sub(r"(\[\w*\])", "", match)
+                pattern = r"\b" + substring + r"\b"
                 result = re.search(pattern, sentence.text)
-                negation_context = NegationContext(input_pack,
-                                        sentence.span.begin + result.span()[0],
-                                        sentence.span.begin + result.span()[1])
+                negation_context = NegationContext(
+                    input_pack,
+                    sentence.span.begin + result.span()[0],
+                    sentence.span.begin + result.span()[1],
+                )
                 negation_context.polarity = False
 
             for match in neg_matches:
-                substring = re.sub(r'(\[\w*\])', '', match)
-                pattern = r'\b' + substring + r'\b'
+                substring = re.sub(r"(\[\w*\])", "", match)
+                pattern = r"\b" + substring + r"\b"
                 result = re.search(pattern, sentence.text)
-                negation_context = NegationContext(input_pack,
-                                        sentence.span.begin + result.span()[0],
-                                        sentence.span.begin + result.span()[1])
+                negation_context = NegationContext(
+                    input_pack,
+                    sentence.span.begin + result.span()[0],
+                    sentence.span.begin + result.span()[1],
+                )
                 negation_context.polarity = True
 
     @classmethod
@@ -207,15 +230,18 @@ class NegationContextAnalyzer(PackProcessor):
         This defines a basic config structure for `NegationContextAnalyzer`.
 
         Following are the keys for this dictionary:
-            - `negation_rules_path`: provides the location of a rule phrases file, which would be
-            used as triggers to detect negation context in sentences.
+            - `negation_rules_path`: provides the location of a rule phrases
+            file, which would be used as triggers to detect negation context
+            in sentences.
 
-            - `pre_negation_rules`: an additional set of pre negation rules that are to be
-            considered along with the default rules that processor uses.
+            - `pre_negation_rules`: an additional set of pre negation rules
+            that are to be considered along with the default rules that
+            processor uses.
             Example: `"pre_negation_rules": ["absence of", "no"]`
 
-            - `post_negation_rules`: an additional set of post negation rules that are to be
-            consdiered along with the default rules that processor uses.
+            - `post_negation_rules`: an additional set of post negation rules
+            that are to be consdiered along with the default rules that
+            processor uses.
             Example: `"post_negation_rules": ["is absent", "not present"]"`
 
         Returns: A dictionary with the default config for this processor.
@@ -228,9 +254,11 @@ class NegationContextAnalyzer(PackProcessor):
 
     def expected_types_and_attributes(self):
         r"""
-        Method to add user specified expected type which would be checked before running the
-        processor if the pipeline is initialized with `enforce_consistency=True` or
-        :meth:`~forte.pipeline.Pipeline.enforce_consistency` was enabled for the pipeline.
+        Method to add user specified expected type which would be checked
+        before running the processor if the pipeline is initialized with
+        `enforce_consistency=True` or
+        :meth:`~forte.pipeline.Pipeline.enforce_consistency` was enabled for
+        the pipeline.
         """
         return {
             "ft.onto.base_ontology.Sentence": set(),
