a28f2a0e02cd7c4049dc7477d9ea61e45f22e6a2
JamesX <james.xiao@mbzuai.ac.ae>
2022-06-03 00:53:18 +0400

    Initial check-in for #18, added Abbreviation of scispacy and unit test. Hyponym will be added later.

---
+++ fortex/health/processors/scispacy_processor.py
@@ -0,0 +1,141 @@
+# Copyright 2022 The Forte Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+SciSpacy Processor
+"""
+from typing import Dict, List, Set
+import importlib
+
+import spacy
+from forte.common import Resources
+from forte.common.configuration import Config
+from forte.data.data_pack import DataPack
+from forte.processors.base import PackProcessor
+
+from scispacy.abbreviation import AbbreviationDetector
+from ftx.medical.clinical_ontology import Hyponym, Abbreviation
+
+__all__ = [
+    "ScispaCyProcessor",
+]
+
+
+class ScispaCyProcessor(PackProcessor):
+    r"""
+    Implementation of this ScispaCyProcessor has been based on ScispaCy
+    pretained models (based on spacy),  A rendition of it that
+    exists on github has been referred to as well.
+
+    Referred repository link:
+    https://pythonlang.dev/repo/allenai-scispacy/
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.extractor = None
+
+    def set_up(self):  # , configs: Config
+        device_num = self.configs["cuda_devices"]
+        self.extractor = spacy.load(  # using Spacy for Classification
+            self.configs.model_name  # model name
+        )
+        self.extractor.add_pipe(self.configs.pipe_name)  # pipe name
+
+    def initialize(self, resources: Resources, configs: Config):
+        super().initialize(resources, configs)
+        self.set_up()
+
+    def _process(self, input_pack: DataPack):
+        r"""
+        ScispaCyProcessor is done on the basis of
+        using SciSpacy and the corresponding
+        trained model for Hyponym, Abbreviation
+
+        """
+
+        path_str, module_str = self.configs.entry_type.rsplit(".", 1)
+
+        mod = importlib.import_module(path_str)
+        entry = getattr(mod, module_str)
+        for entry_specified in input_pack.get(entry_type=entry):
+
+            doc = self.extractor(entry_specified.text)
+
+            print("Abbreviation", "\t", "Definition")
+            list_of_abrvs = []
+            for abrv in doc._.abbreviations:
+                print(
+                    f"{abrv} \t ({abrv.start}, {abrv.end}) {abrv._.long_form}"
+                )
+                tmpAbrv = Abbreviation(
+                    pack=input_pack, begin=abrv.start, end=abrv.end
+                )
+                tmpAbrv.long_form = abrv._.long_form
+                # tmpAbrv.text = "" + abrv
+                list_of_abrvs.append(tmpAbrv)
+
+    @classmethod
+    def default_configs(cls):
+        r"""
+        This defines a basic config structure for `ICDCodingProcessor`.
+
+        Following are the keys for this dictionary:
+         - `entry_type`: input entry type,
+         - `model_name`: the higgingface transformer model name to be
+                         used for classification,
+
+        Returns: A dictionary with the default config for this processor.
+        """
+        return {
+            "entry_type": "ft.onto.base_ontology.Document",
+            "attribute_name": "classification",
+            "multi_class": True,
+            "model_name": "en_core_sci_sm",
+            "pipe_name": "abbreviation_detector",
+            "cuda_devices": -1,
+        }
+
+    def expected_types_and_attributes(self):
+        r"""
+        Method to add user specified expected type which would be checked
+        before running the processor if the pipeline is initialized with
+        `enforce_consistency=True` or
+        :meth:`~forte.pipeline.Pipeline.enforce_consistency` was enabled for
+        the pipeline.
+        """
+        return {
+            "ft.onto.base_ontology.Document": set(),
+            "forte.data.ontology.top.Annotation": set(),
+        }
+
+    def record(self, record_meta: Dict[str, Set[str]]):
+        r"""
+        Method to add output type record of `ICDCodeProcessor` which
+        is `"ftx.medical.clinical_ontology.MedicalArticle"` with attributes:
+         `icd_version` and `icd_code`
+        to :attr:`forte.data.data_pack.Meta.record`.
+
+        Args:
+            record_meta: the field in the datapack for type record that need to
+                fill in for consistency checking.
+        """
+        record_meta["ftx.medical.clinical_ontology.Abbreviation"] = {
+            "long_form",
+        }
+        if self.configs.entry_type in record_meta:
+            record_meta[self.configs.entry_type].add(
+                self.configs.attribute_name
+            )
+        else:
+            record_meta[self.configs.entry_type] = {self.configs.attribute_name}
---
+++ tests/forte_medical/processors/scispacy_processor_test.py
@@ -0,0 +1,66 @@
+# Copyright 2022 The Forte Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Unit tests for ScispaCyProcessor
+"""
+
+import unittest
+
+from forte.data.data_pack import DataPack
+from forte.data.readers import StringReader
+from forte.pipeline import Pipeline
+
+from ftx.medical.clinical_ontology import Hyponym, Abbreviation
+
+from forte_medical.processors.scispacy_processor import (
+    ScispaCyProcessor,
+)
+
+
+class TestScispaCyProcessor(unittest.TestCase):
+    def setUp(self):
+        self.nlp = Pipeline[DataPack](enforce_consistency=False)  #  True
+        self.nlp.set_reader(StringReader())
+        config = {
+            "entry_type": "ft.onto.base_ontology.Document",
+            "attribute_name": "classification",
+            "multi_class": True,
+            "model_name": "en_core_sci_sm",
+            "pipe_name": "abbreviation_detector",
+            "cuda_devices": -1,
+        }
+
+        self.nlp.add(ScispaCyProcessor(), config=config)  # , config=config
+        self.nlp.initialize()
+
+    def test_ScispaCy_processor(self):
+        sentences = [
+            "Spinal and bulbar muscular atrophy (SBMA) is an \
+            inherited motor neuron disease caused by the expansion \
+            of a polyglutamine tract within the androgen receptor (AR). \
+            SBMA can be caused by this easily."
+        ]
+        document = "".join(sentences)
+        print(document)
+        pack = self.nlp.process(document)
+
+        expected_longform = [
+            "Spinal and bulbar muscular atrophy",
+            "androgen receptor",
+            "Spinal and bulbar muscular atrophy",
+        ]
+
+        for idx, abv_item in enumerate(pack.get(Abbreviation)):
+            # print(abv_item.long_form.text)
+            self.assertEqual(abv_item.long_form.text, expected_longform[idx])
