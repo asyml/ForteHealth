60bb3919d4f96f246f328f5f8a86bc771f6bba04
KiaLAN <1139479308@qq.com>
2022-06-15 17:28:46 +0800

    remove superfluous comments

--- examples/icd_code_prediction/icd_coding.py
+++ examples/icd_code_prediction/icd_coding.py
@@ -13,43 +13,30 @@ from ft.onto.base_ontology import (
 )
 from fortex.spacy import SpacyProcessor
 
-# import MedicalArticle to get article data from data pack
 from ftx.medical.clinical_ontology import MedicalArticle
-
-# import mimic3 dataset
 from fortex.health.readers.mimic3_note_reader import Mimic3DischargeNoteReader
-
-# import ICD coding processor
 from fortex.health.processors.icd_coding_processor import (
     ICDCodingProcessor,
 )
 
+
 def main(
-    input_path: str, # Path to mimic3 data if use_mimic3_reader=True else path to note directory
-    output_path: str, # Path to output directory
-    max_packs: int = -1, # Max number of notes to read from mimic3 dataset. Set to -1 to read all.
-    use_mimic3_reader: bool = True, # Read from mimic3 dataset or plain text
+    input_path: str,  # Path to mimic3 data if use_mimic3_reader=True else path to notes directory
+    output_path: str,  # Path to output directory
+    max_packs: int = -1,  # Max number of notes to read from mimic3 dataset. Set to -1 to read all.
+    use_mimic3_reader: bool = True,  # Read from mimic3 dataset or plain text
 ):
-    # create a pipeline
     pl = Pipeline[DataPack]()
 
-    # select reader: read from mimic3 dataset or from files
     if use_mimic3_reader is False:
         pl.set_reader(PlainTextReader())
     else:
-        pl.set_reader(
-            Mimic3DischargeNoteReader(), config={"max_num_notes": max_packs}
-        )
+        pl.set_reader(Mimic3DischargeNoteReader(), config={"max_num_notes": max_packs})
 
-    # load config
     config = Config(yaml.safe_load(open("config.yml", "r")), None)
 
-    # add processors
-    # use spacy for tokenizing
     pl.add(SpacyProcessor(), config.Spacy)
-    # predict icd code
     pl.add(ICDCodingProcessor(), config.ICD)
-    # add writer to save the output to the disk
     pl.add(
         PackIdJsonPackWriter(),
         {
@@ -61,47 +48,39 @@ def main(
         },
     )
 
-    # initialize the pipeline before using it
     pl.initialize()
 
-    # process data
     packs = pl.process_dataset(input_path)
     for pack in packs:
         showData(pack)
 
+
 def showData(pack: DataPack):
-    # show the processed data
-    
     # The ICD processor predicts ICD code for each article.
     # The result is stored as article.icd_code.
     # The articles are packed into DataPack.
     # Therefore, we first extract articles from DataPack and then get their ICD codes.
-    
-    # extract ariticle
-    for article in pack.get(MedicalArticle):
-        # get original text
-        article_text = article.text
 
-        # get parsed tokens
-        tokens = [
-            (token.text) for token in pack.get(Token, article)
-        ]
+    for article in pack.get(MedicalArticle):
+        article_text = article.text
+        tokens = [(token.text) for token in pack.get(Token, article)]
 
         # get the ICD code and its coding version
         icd_code = article.icd_code
         icd_version = article.icd_version
-        
+
         print(colored("Article:", "red"), article_text, "\n")
         print(colored("Tokens:", "red"), tokens, "\n")
         print(colored(f"ICD-{icd_version} Code:", "cyan"), icd_code, "\n")
 
         input(colored("Press ENTER to continue...\n", "green"))
 
-# Examples: 
+
+# Examples:
 #
 # Read from MIMIC3:
 # python icd_coding.py /path/to/mimiciii/1.4/NOTEEVENTS.csv.gz /path_to_sample_output 1000 True
-# 
+#
 # Read from sample_data:
 # python icd_coding.py sample_data/ /path_to_sample_output 1000 False
 main(sys.argv[1], sys.argv[2], int(sys.argv[3]), sys.argv[4].lower() == "true")
