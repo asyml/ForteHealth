f453e60f94db9ccdd9b45132f0de26479e0eec40
KiaLAN <1139479308@qq.com>
2022-07-01 18:44:37 +0800

    shorten comment

--- fortex/health/processors/coreference_processor.py
+++ fortex/health/processors/coreference_processor.py
@@ -35,12 +35,12 @@ __all__ = [
 
 class CoreferenceProcessor(PackProcessor):
     r"""
-    Implementation of this CoreferenceProcessor has been based on huggingface
-    NeuralCoref. You can find more details in the original repo.
+    Implementation of this CoreferenceProcessor has been based on
+    huggingface NeuralCoref. You can find more details in the original repo.
 
     Note that the NeuralCoref package from PyPI uses a dated spaCy
-    version (2.1), which can cause segmentation fault with the spaCy we use (2.3).
-    Please install NeuralCoref by building from source.
+    version (2.1), which can cause segmentation fault with the spaCy
+    we use (2.3). Please install NeuralCoref by building from source.
 
     Referred repository link:
     https://github.com/huggingface/neuralcoref
@@ -59,7 +59,10 @@ class CoreferenceProcessor(PackProcessor):
                 "haven't called the initialization function."
             )
 
-        model = configs.model if configs.model != "use_default_model" else True
+        if configs.model != "use_default_model":
+            model = configs.model
+        else:
+            model = True
         cfg_inference = configs.cfg_inference
         neuralcoref.add_to_pipe(
             self.spacy_nlp, model=model, cfg_inference=cfg_inference
@@ -104,11 +107,14 @@ class CoreferenceProcessor(PackProcessor):
                             mention.start_char + entry_specified.begin,
                             mention.end_char + entry_specified.begin,
                         )
+
+                        # TODO: remove assertion?
                         assert (
-                            mention.text == mention_text,  # TODO: remove assertion?
+                            mention.text == mention_text,
                             f"The processor extracted mention {mention.text}"
-                            f" which is different from the original mention {mention_text}."
-                            f"The offeset calculation is wrong.",
+                            f" which is different from the original mention"
+                            f" {mention_text}. The offeset calculation"
+                            f" is wrong.",
                         )
                         mentions.append(mention)
 
@@ -131,18 +137,20 @@ class CoreferenceProcessor(PackProcessor):
         - `mention_type`: The type of members in `CoreferenceGroup`.
           Default: `"ftx.medical.clinical_ontology.MedicalEntityMention"`.
           It can also be set to `"ft.onto.base_ontology.EntityMention"`.
-        - `lang`: The SpaCy pipeline to be used. The pipeline does the preprocessing
-          steps for NeuralCoref.
+        - `lang`: The SpaCy pipeline to be used. The pipeline does the
+          preprocessing steps for NeuralCoref.
           Default: `"en_core_web_sm"`.
-        - `model`: the neural net model to be used by NeuralCoref. If set to
-          `"use_default_model"`, a pre-trained neural net will be downloaded and cached.
-          If set to your customized model, the model needs to be a tuple containing a
-          `single_model` and a `pairs_model`. See `NeuralCoref.Model` method in
+        - `model`: the neural net model to be used by NeuralCoref. If set
+          to `"use_default_model"`, a pre-trained neural net will be
+          downloaded and cached.
+          If set to your customized model, the model needs to be a tuple
+          containing a `single_model` and a `pairs_model`.
+          See `NeuralCoref.Model` method in
           https://github.com/huggingface/neuralcoref/blob/master/neuralcoref/neuralcoref.pyx
           for reference of how the default model is defined.
           Default: `"use_default_model"`.
-        - `cfg_inference`: A dict containing the inference configs of NeuralCoref. See
-          `get_default_cfg_inference` for default values, and see
+        - `cfg_inference`: A dict containing the inference configs of
+          NeuralCoref. See `get_default_cfg_inference` for default values, and see
           https://github.com/huggingface/neuralcoref/blob/master/README.md#parameters
           for the meaing of these parameters.
 
@@ -162,31 +170,37 @@ class CoreferenceProcessor(PackProcessor):
         This defines the default inference config of NeuralCoref.
 
         Following are the keys for this dictionary:
-         - `greedyness` (`float`): A number between 0 and 1 determining how greedy
-            the model is about making coreference decisions
-            (more greedy means more coreference links).
-            Default: `0.5`.
-         - `max_dist` (`int`): How many mentions back to look when considering possible
-            antecedents of the current mention. Decreasing the value will cause
-            the system to run faster but less accurately.
-            Default: `50`.
-         - `max_dist_match` (`int`): The system will consider linking the current mention
-            to a preceding one further than max_dist away if they share a noun or
-            proper noun. In this case, it looks max_dist_match away instead.
-            Default: `500`.
-         - `blacklist` (`bool`): Should the system resolve coreferences for pronouns in the
-            following list: ["i", "me", "my", "you", "your"].
-            Default `True`.
-         - `store_scores` (`bool`): Should the system store the scores for the coreferences
-            in annotations.
-            Default: `True`
-         - `conv_dict` (`dict(str, list(str))`): A conversion dictionary that you can use
-            to replace the embeddings of rare words (keys) by an average of the embeddings
-            of a list of common words (values). Ex: `conv_dict={"Angela": ["woman", "girl"]}`
-            will help resolving coreferences for Angela by using the embeddings for the more
-            common woman and girl instead of the embedding of Angela.
-            This currently only works for single words (not for words groups).
-            Default: `None`.
+        - `greedyness` (`float`): A number between 0 and 1 determining
+           how greedy the model is about making coreference decisions
+           (more greedy means more coreference links).
+           Default: `0.5`.
+        - `max_dist` (`int`): How many mentions back to look when
+           considering possible antecedents of the current mention.
+           Decreasing the value will cause the system to run faster
+           but less accurately.
+           Default: `50`.
+        - `max_dist_match` (`int`): The system will consider linking
+           the current mention
+           to a preceding one further than max_dist away if they share
+           a noun or proper noun. In this case, it looks max_dist_match
+           away instead.
+           Default: `500`.
+        - `blacklist` (`bool`): Should the system resolve coreferences
+           for pronouns in the following list: ["i", "me", "my", "you", "your"].
+           Default `True`.
+        - `store_scores` (`bool`): Should the system store the scores
+           for the coreferences in annotations.
+           Default: `True`
+        - `conv_dict` (`dict(str, list(str))`): A conversion dictionary
+           that you can use
+           to replace the embeddings of rare words (keys) by an average
+           of the embeddings of a list of common words (values).
+           Ex: `conv_dict={"Angela": ["woman", "girl"]}`
+           will help resolving coreferences for Angela by using the
+           embeddings for the more common woman and girl instead of the
+           embedding of Angela.
+           This currently only works for single words (not for words groups).
+           Default: `None`.
 
         Returns: A dictionary with the default inference config of NeuralCoref.
         """
