451c4882342576d54e4a696901ccca6cc85a1f58
Leolty <569359974@qq.com>
2022-07-22 01:26:23 +0800

    Search engine and related utils

---
+++ examples/label_example/search_utils.py
@@ -0,0 +1,57 @@
+'''
+this file defines search functions for searching data in elasticsearch.
+'''
+
+
+def all_search(es, index: str) -> dict:
+    """
+    Args:
+        es: Elasticsearch client instance.
+        index: Name of the index we are going to use.
+        size: Number of results returned in each search.
+    """
+    # search query
+    body = {"query": {"match_all": {}}}
+
+    res = es.search(index=index, body=body)
+
+    return res
+
+
+def index_search(es, index: str, keywords: str) -> dict:
+    """
+    Args:
+        es: Elasticsearch client instance.
+        index: Name of the index we are going to use.
+        keywords: Search keywords.
+        from_i: Start index of the results for pagination.
+        size: Number of results returned in each search.
+    """
+    # search query
+    body = {
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "query_string": {
+                            "query": keywords,
+                            "fields": ["content"],
+                            "default_operator": "AND",
+                        }
+                    }
+                ],
+            }
+        },
+        "highlight": {
+            "pre_tags": [' <font color = "#dc3023">'],
+            "post_tags": ["</font>"],
+            "fields": {"content": {}},
+        },
+        # "from": from_i,
+        # "size": size,
+        "aggs": {"match_count": {"value_count": {"field": "_id"}}},
+    }
+
+    res = es.search(index=index, body=body)
+
+    return res
---
+++ examples/label_example/sqlite_utils.py
@@ -0,0 +1,79 @@
+"""
+this file defines sqlite3 related utils for inserting data to the database of stave.
+"""
+import json
+from typing import List
+from stave_backend.lib.stave_session import StaveSession
+import sqlite3
+
+
+def sqlite_insert(conn, table, row):
+    """
+    Args:
+        conn: connection
+        table: table name
+        row: inserted item
+    """
+    cols: str = ", ".join('"{}"'.format(col) for col in row.keys())
+    vals: str = ", ".join(":{}".format(col) for col in row.keys())
+    sql: str = 'INSERT INTO "{0}" ({1}) VALUES ({2})'.format(table, cols, vals)
+    cursor = conn.cursor()
+    cursor.execute(sql, row)
+    conn.commit()
+    return cursor.lastrowid
+
+
+def create_links(url_stub: str, ids: List[int]) -> List[str]:
+    """
+    Args:
+        url_stub: url of stave
+        ids: the doc ids of the reports
+    """
+    links: List[str] = []
+
+    url_stub: str = url_stub.strip("/")
+    for temp_idm in ids:
+        links.append(
+            f"<a href={url_stub}/documents/{temp_idm}><font size='6'>Report #{temp_idm}</font></a>"
+        )
+    return links
+
+
+def get_json(path: str):
+    """
+    Args:
+        path: the file path of the json file
+    """
+    file_obj = open(path)
+    data = json.load(file_obj)
+    file_obj.close()
+    return data
+
+
+def update_stave_db(default_project_json, config):
+    """
+    Args:
+        default_project_json: the ontology configuration file
+        config: the configuration of Stave, including url, name, password, etc.
+    """
+    project_id_base = 0
+    with StaveSession(url=config.Stave.url) as session:
+        session.login(username=config.Stave.username, password=config.Stave.pw)
+
+        projects = session.get_project_list().json()
+        project_names = [project["name"] for project in projects]
+
+        if (
+            default_project_json["name"] in project_names
+            ):
+
+            base_project = [
+                proj
+                for proj in projects
+                if proj["name"] == default_project_json["name"]
+            ][0]
+            return base_project["id"]
+
+        resp1 = session.create_project(default_project_json)
+        project_id_base = json.loads(resp1.text)["id"]
+    return project_id_base
