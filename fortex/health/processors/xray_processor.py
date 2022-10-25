e91e986bb58c0bb437cbd12f248e30039aa350bd
bhaskar2443053 <bhaskar2443053@gmail.com>
2022-10-25 13:36:55 +0400

    template format processor

--- fortex/health/processors/xray_processor.py
+++ fortex/health/processors/xray_processor.py
@@ -12,16 +12,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-XRAY Processor
+XRAY_Processor Processor
 """
-
-from forte.data.data_pack import DataPack
+from transformers import pipeline
 from forte.common.resources import Resources
 from forte.common.configuration import Config
 from forte.processors.base import PackProcessor
 from transformers import AutoFeatureExtractor, AutoModelForImageClassification
 from ft.onto.base_ontology import Classification
+
 import torch
+
 
 __all__ = [
     "XRAY_Processor",
@@ -29,31 +30,103 @@ __all__ = [
 
 
 class XRAY_Processor(PackProcessor):
+    r"""
+    Implementation of this XRAY_Processor has been based on the fine-tuned
+    version of google/vit-base-patch16-224-in21k 
+    on the chest-xray-pneumonia dataset.
+    
+    The finetuned model nickmuchi/vit-finetuned-chest-xray-pneumonia (of huggingface transformers)
+    achieves the following results on the evaluation set:
+
+    Loss: 0.1271
+    Accuracy: 0.9551
+
+    Referred link:
+    https://huggingface.co/nickmuchi/vit-finetuned-chest-xray-pneumonia#vit-finetuned-chest-xray-pneumonia
     """
-    An image processor for classification.
-    """
+
+    def __init__(self):
+        super().__init__()
+        self.extractor = None
+
+    def set_up(self, configs: Config):  
+        device_num = self.configs["cuda_devices"]
+        self.extractor = pipeline(  
+            "image-classification",
+            model=self.configs.model_name,
+            feature_extractor=self.configs.model_name,
+            framework="pt",
+            device=device_num,
+        )
 
     def initialize(self, resources: Resources, configs: Config):
         super().initialize(resources, configs)
-        self.extractor = AutoFeatureExtractor.from_pretrained(
-            "nickmuchi/vit-finetuned-chest-xray-pneumonia"
-        )
-        self.model = AutoModelForImageClassification.from_pretrained(
-            "nickmuchi/vit-finetuned-chest-xray-pneumonia"
-        )
+        self.set_up(configs)
 
     def _process(self, input_pack: DataPack):
+        r"""
+        The model predicts the probablity score for
+        the two classes 'PNEUMONIA' and 'NORMAL', based on
+        an xray image input.
 
+        """
         image_data = input_pack.image
+        pil_img=PIL.Image.fromarray(image_data)
+        out = self.extractor(pil_img)
+        out_dict={}
+        for i in out:
+            out_dict[i['label']]=i['score']
+        class_labels: Classification = Classification(
+                pack=input_pack
+            )
+        class_labels.classification_result = out_dict
 
-        extract = self.extractor(image_data, return_tensors="pt")
+        
 
-        with torch.no_grad():
-            logits = self.model(**extract).logits
-            prob = torch.nn.functional.softmax(logits, dim=1)
-        predicted_label = logits.argmax(-1).item()
+    @classmethod
+    def default_configs(cls):
+        r"""
+        This defines a basic config structure for `XRAY_Processor`.
 
-        output = self.model.config.id2label[predicted_label]
-        prob_cls = prob[0].numpy()[predicted_label]
-        class_labels: Classification = Classification(pack=input_pack)
-        class_labels.classification_result = {output: prob_cls}
+        Following are the keys for this dictionary:
+         - `model_name`: the higgingface transformer model name to be
+                         used for classification,
+         - `cuda_devices`: This the GPU device id, if set to "-1" CPU
+                         will be used for the computation.
+
+        Returns: A dictionary with the default config for this processor.
+        """
+        return {
+            "model_name": "nickmuchi/vit-finetuned-chest-xray-pneumonia",
+            "cuda_devices": -1
+        }
+    def expected_types_and_attributes(self):
+        r"""
+        Method to add user specified expected type which would be checked
+        before running the processor if the pipeline is initialized with
+        `enforce_consistency=True` or
+        :meth:`~forte.pipeline.Pipeline.enforce_consistency` was enabled for
+        the pipeline.
+        """
+        return {
+            "from forte.data.ontology.top.Payload": DataPack,
+        }
+
+    def record(self, record_meta: Dict[str, float]):
+        r"""
+        Method to add output type record of `XRAY_Processor` which
+        is `ft.onto.base_ontology.Classification` with attribute `classification_result`
+        to :attr:`forte.data.data_pack.Meta.record`.
+
+        Args:
+            record_meta: the field in the datapack for type record that need to
+                fill in for consistency checking.
+        """
+        record_meta["ft.onto.base_ontology.Classification"] = [
+        {
+          "name": "classification_result",
+          "type": "Dict",
+          "key_type": "str",
+          "value_type": "float"
+        }
+      ]
