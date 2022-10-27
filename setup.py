559b37b2131c266687296c3fc5f484ea932559ef
bhaskar2443053 <bhaskar2443053@gmail.com>
2022-10-27 14:55:06 +0400

    add pillow to setup

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setuptools.setup(
             "testfixtures",
             "transformers==4.2.2",
             "protobuf==3.19.4",
-            "Pillow==9.2.0"
+            "Pillow==8.4.0"
         ],
         "scispacy_processor": [
             "scispacy==0.5.0",
