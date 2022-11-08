a3f951d689daf1f77b06f76e64516a0972c24d5e
nikhilranjan7 <nikhilranjan7@gmail.com>
2022-11-08 12:21:25 +0400

    add abbreviation detection

--- examples/medical_text_understanding/medical_text_understanding.py
+++ examples/medical_text_understanding/medical_text_understanding.py
@@ -22,16 +22,10 @@ from scispacy_processor import (
 )
 
 def main(
-    input_path: str,  # Path to mimic3 data if use_mimic3_reader=True else path to notes directory
-    output_path: str,  # Path to output directory
+    input_path: str,
 ):
     pl = Pipeline[DataPack]()
     pl.set_reader(PlainTextReader())
-
-    # pl.add(
-    #     SpacyProcessor(),
-    #     {"processors": ["sentence"], "lang": "en_core_sci_sm"},
-    # )
     pl.add(
         ScispaCyProcessor(),
         {
@@ -43,16 +37,6 @@ def main(
             "gpu_id": 0,
         },
     )
-    # pl.add(
-    #     PackIdJsonPackWriter(),
-    #     {
-    #         "output_dir": output_path,
-    #         "indent": 2,
-    #         "overwrite": True,
-    #         "drop_record": True,
-    #         "zip_pack": True,
-    #     },
-    # )
 
     pl.initialize()
 
@@ -60,34 +44,9 @@ def main(
 
     for pack in packs:
         for idx, abv_item in enumerate(pack.get(Abbreviation)):
-            print(abv_item.long_form.text)
-
-
-def show_data(pack: DataPack):
-    # The ICD processor predicts ICD code for each article.
-    # The result is stored as article.icd_code.
-    # The articles are packed into DataPack.
-    # Therefore, we first extract articles from DataPack and then get their ICD codes.
-
-    for article in pack.get(MedicalArticle):
-        article_text = article.text
-
-        # get the ICD code and its coding version
-        icd_code = article.icd_code
-        icd_version = article.icd_version
-
-        print(colored("Article:", "red"), article_text, "\n")
-        print(colored(f"ICD-{icd_version} Code:", "cyan"), icd_code, "\n")
-
-        input(colored("Press ENTER to continue...\n", "green"))
-
+            print(colored("Long form text:", "red"), colored( abv_item.long_form.text, "blue"), "\n")
 
 # Examples:
-#
-# Read from MIMIC3:
-# python icd_coding.py /path/to/mimiciii/1.4/NOTEEVENTS.csv.gz /path_to_sample_output 1000 True
-#
 # Read from sample_data:
-# python medical_text_understanding.py sample_data/ /path_to_sample_output 1000 False
-# python medical_text_understanding.py sample_data/ .
-main(sys.argv[1], sys.argv[2])
+# python medical_text_understanding.py sample_data/
+main(sys.argv[1])
