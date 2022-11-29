c96791de9c116c4056ea415bf6484b2649401a83
nikhilranjan7 <nikhilranjan7@gmail.com>
2022-11-29 15:26:24 +0400

    fix name

--- tests/fortex/health/processors/temporal_mention_tagging_processor_test.py
+++ tests/fortex/health/processors/temporal_mention_tagging_processor_test.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Unit tests for TemporalMentionTaggingAndNormalizingProcessor
+Unit tests for TemporalMentionTaggingProcessor
 """
 
 import unittest
