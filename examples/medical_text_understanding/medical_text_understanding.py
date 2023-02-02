66212f403f09bfddf2de44f1b70287730a591ef9
nikhilranjan7 <nikhilranjan7@gmail.com>
2023-02-02 15:40:36 +0400

    add more examples

--- examples/medical_text_understanding/medical_text_understanding.py
+++ examples/medical_text_understanding/medical_text_understanding.py
@@ -4,11 +4,6 @@ from termcolor import colored
 from forte.data.data_pack import DataPack
 from forte.data.readers import PlainTextReader
 from forte.pipeline import Pipeline
-from forte.processors.writers import PackIdJsonPackWriter
-
-from ft.onto.base_ontology import (
-    Token,
-)
 
 from ftx.medical.clinical_ontology import Hyponym, Abbreviation
 from fortex.health.processors.scispacy_processor import (
@@ -21,7 +16,7 @@ def main(
 ):
     pl = Pipeline[DataPack]()
     pl.set_reader(PlainTextReader())
-    if process == "abb":
+    if process == "abbreviation":
         pl.add(
             ScispaCyProcessor(),
             {
@@ -33,7 +28,7 @@ def main(
                 "gpu_id": 0,
             },
         )
-    elif process == "hyp":
+    elif process == "hyponym":
         pl.add(
             ScispaCyProcessor(),
             {
@@ -45,30 +40,36 @@ def main(
                 "gpu_id": 0,
             },
         )
+    else:
+        raise ValueError("""Please use hyponym or abbreviation as argument. e.g., 
+            Abbreviation:
+                python medical_text_understanding.py sample_data_abb/ abbreviation
+            Hyponym detection:
+                python medical_text_understanding.py sample_data_hyp/ hyponym""")
 
     pl.initialize()
 
     packs = pl.process_dataset(input_path)
 
     for pack in packs:
-        if process == "abb":
+        if process == "abbreviation":
             print(colored("Input Sentence:", "blue"), colored( pack.text, "red"))
             for idx, abv_item in enumerate(pack.get(Abbreviation)):
                 print(colored("Abbreviation:", "blue"), colored( abv_item.text, "green"))
                 print(colored("Long form text:", "blue"), colored( abv_item.long_form.text, "green"), "\n")
 
-        elif process == "hyp":
+        elif process == "hyponym":
             print(colored("Input Sentence:", "blue"), colored( pack.text, "red"))
             for idx, detected in enumerate(pack.get(Hyponym)):
                 print(colored("hyponym_link:", "blue"), colored(detected.hyponym_link, "green"))
                 print(colored("hypernym:", "blue"), colored(detected.general, "green"))
-                print(colored("hyponym:", "blue"), colored(detected.specific, "green"))
+                print(colored("hyponym:", "blue"), colored(detected.specific, "green")+"\n\n")
 # Examples:
 '''
 Read from sample_data:
 Abbreviation:
-    python medical_text_understanding.py sample_data_abb/ abb
+    python medical_text_understanding.py sample_data_abb/ abbreviation
 Hyponym detection:
-    python medical_text_understanding.py sample_data_hyp/ hyp
+    python medical_text_understanding.py sample_data_hyp/ hyponym
 '''
 main(sys.argv[1], sys.argv[2])
